dcf4ddf41c229345127701d80f7060c9dae7dbc8
Vagner dos Santos <30627980+vagnersant@users.noreply.github.com>
2024-09-05 23:47:04 +0000

    2º commit

---
+++ src/components/PostModelo.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        props: {
+            titulo: String,
+            corpoDoTexto: String,
+            autor: String
+        }
+    }
+</script>
+
+<template>
+    <div class="post">
+        <img src="../assets/logo.svg">
+        <h2>{{titulo}}</h2>
+        <p>
+          {{corpoDoTexto}}
+        </p>
+        <p class="autor">
+          por {{autor}}
+        </p>
+      </div>
+</template>
+
+<style>
+
+</style>
